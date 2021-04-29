bb2820932924ad5f508ba97fbff651aa8ec4ebc9
Tom Sorteberg <66091315+tsorteberg@users.noreply.github.com>
2021-04-29 16:18:13 -0500

    Updated index.html.
    I updated index.html to map to userSignIn controllers and added JavaScript validation.

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -11,7 +11,6 @@ package dmacc.controller;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 
-import javax.swing.JOptionPane;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
